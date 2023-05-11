41f9c5f5f3bc0bdb0e01b34c7b8ac986873fcab4
Rafli Pasya <androgaming1912@gmail.com>
2023-05-11 10:42:53 +0700

    ubah warna jadi primary

--- resources/views/traceability/electric/antenna/ng.blade.php
+++ resources/views/traceability/electric/antenna/ng.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12 col-sm-12 col-md-12">
             <h5 class="mb-3 text-right"><span class="badge badge-dark" style="border-radius: 7px !important">Welcome,
                     {{ auth()->user()->name }}</span></h5>
-            <div class="shadow hero bg-white text-dark rounded-3">
+            <div class="shadow hero bg-white text-dark rounded-3 mb-5">
                 <div class="hero-inner">
                     <div class="row mb-3 w-100">
                         <div class="col-12 d-flex justify-content-center w-100">
@@ -14,31 +14,20 @@
                     </div>
                     <div class="row mb-3 w-100">
                         <div class="col-12 d-flex justify-content-center w-100">
-                            <h1 class="text-danger" style="font-weight: 400 !impoertant">Part Rusak</h1>
+                            <h1 class="text-danger" style="font-weight: 400 !important;">Part Rusak</h1>
                         </div>
                     </div>
-
                     <form class="row mb-3 w-100">
                         <input id="code" type="text" class="form-control" name="code" tabindex="1"
                             placeholder="scan part here..." required autofocus>
                     </form>
-                    <div class="row mb-3 w-100 ">
-                        <h5 class="text-left text-primary">Parts Scanned | Rusak</h5>
-                        <div class=" col-12 list-group mt-3" style="max-height: 7rem; width: 100%; overflow-y: scroll; overflow-x: hidden;" >
-                            <li class="list-group-item">
-                                <h4 class="text-dark">081920192819281</h4>
-                            </li>
-                            <li class="list-group-item">
-                                <h4 class="text-dark">081920192819281</h4>
-                            </li>
-                        </div>
-                    </div>
                 </div>
                 
             </div>
 
         </div>
     </div>
+    
 @endsection
 
 @section('custom-script')
